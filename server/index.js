1b87defea2661b64414fead88f55d2e10f32cf0f
medcloud <medcloud.pk@gmail.com>
2024-03-23 11:16:09 +0500

    first commit

--- server/index.js
+++ server/index.js
@@ -1,18 +1,14 @@
-import express from 'express';
-import cors from 'cors';
-import connectDB from './config/db.js'; // Assuming connectDB is exported from db.js as an ES module
-import dotenv from 'dotenv';
-import studentsRoutes  from './routes/studentsRoutes.js'
-import path  from 'path';
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js"; // Assuming connectDB is exported from db.js as an ES module
+import dotenv from "dotenv";
+import studentsRoutes from "./routes/studentsRoutes.js";
+import path from "path";
 
 const app = express();
 
 // Enable CORS
-app.use(cors(
-  
-));
-
-
+app.use(cors());
 
 // Configure env
 dotenv.config();
@@ -22,21 +18,19 @@ app.use(express.json());
 // Database config
 connectDB();
 
+app.get("/", (res, req) => {
+  res.json("server is running ");
+});
 
-app.get('/',(req,res)=>{
-  app.use(express.static(path.resolve(__dirname,"client","build")));
-  res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-})
-
+// app.get('/',(req,res)=>{
+//   app.use(express.static(path.resolve(__dirname,"client","build")));
+//   res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+// })
 
 // Serve uploaded images statically
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
-app.use('/api', studentsRoutes);
-
-
-
-
+app.use("/api", studentsRoutes);
 
 const PORT = process.env.PORT || 4000;
 
