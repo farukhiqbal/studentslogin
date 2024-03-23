04d3424b229f764b02bf83dbe5e7eae02a4844b9
medcloud <medcloud.pk@gmail.com>
2024-03-23 12:11:52 +0500

    final

--- server/index.js
+++ server/index.js
@@ -10,12 +10,6 @@ const app = express();
 // Enable CORS
 app.use(cors());
 
-app.use(cors({
-  origin: "https://studentslogin.vercel.app",
-  methods: ["POST", "GET", "DELETE", "PUT"],
-  credentials: true
-}));
-
 // Configure env
 dotenv.config();
 
